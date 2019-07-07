0435d9c11e8d6d68d33fec50feb098d18c956abd
EEstevanell <e.estevanell@estudiantes.matcom.uh.cu>
2019-07-07 17:05:28 -0400

    data consistency adquired

--- src/main.py
+++ src/main.py
@@ -2,66 +2,76 @@ import gensim as gs
 import data_manager as dm
 import settings as st
 
-documents = [ "Human machine interface for lab abc computer applications",
-              "The EPS user interface management system",
-              "A survey of user opinion of computer system response time",
-              "System and human system engineering testing of EPS",
-              "Relation of user perceived response time to error measurement",
-              "The generation of random binary unordered trees",
-              "The intersection graph of paths in trees",
-              "Graph minors IV Widths of trees and well quasi ordering",
-              "Graph minors A survey"]
-
-stoplist = set('for a of the and to in'.split())
-texts = [[word for word in document. lower() . split() if word not in stoplist] for document in documents]
-all_tokens = sum(texts, [])
-tokens_once = set(word for word in set(all_tokens) if all_tokens. count(word) == 1)
-texts = [[word for word in text if word not in tokens_once] for text in texts]
-
-dictionary = gs.corpora.Dictionary(texts)
-dictionary.save('src/tmp/deerwester.dict')
-
-new_doc = "Human computer interaction"
-new_vec = dictionary.doc2bow(new_doc. lower() . split())
-
-corpus = [dictionary.doc2bow(text) for text in texts]
-# gs.corpora.MmCorpus.serialize('src/tmp/deerwester.mm' , corpus)
-
-# corpus = gs.corpora.MmCorpus('src/tmp/deerwester.mm' )
-tfidf = gs.models.TfidfModel(corpus)
-
-corpus_tfidf = tfidf[corpus]
-
-lsi = gs.models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=2)
-corpus_lsi = lsi[corpus_tfidf]
-lsi.save('src/tmp/model.lsi' )
-
-lsi.save('src/tmp/model.lsi' )
-lsi = gs.models.LsiModel.load('src/tmp/model.lsi' )
-
-doc = "Human computer interaction"
-vec_bow = dictionary.doc2bow(doc.lower().split())
-vec_lsi = lsi[vec_bow]
-
-index = gs.similarities.MatrixSimilarity(lsi[corpus])
-index.save('src/tmp/deerwester.index' )
-index = gs.similarities.MatrixSimilarity.load('src/tmp/deerwester.index' )
-
-sims = index[vec_lsi]
-sims = sorted(enumerate(sims), key=lambda item: -item[0])
-print(sims)
-
 class LawAnalyzer():
     def __init__(self, laws_json):
         self.new_law = dm.get_new_law(laws_json)
         self.old_law = dm.get_old_law(laws_json)
+        self.stoplist = set('for a of the and to in'.split())
 
     def save_law(self, law):
-        pass
+        l_id = law['id']
+        directory = st.get_law_directory(l_id)
+
+        #if law already was saved then don't append in index table
+        laws = self.get_all_laws()
+        if not str(l_id) in laws:
+            #save law id in the laws index table
+            with open(f"{st.law_index_file}",'a') as index_file:
+                print(l_id, file = index_file)
+
+        with open(st.get_law_index_file(l_id), 'w') as index_file:
+            for key in law.keys():
+                if key != "id":
+                    #save article address in the law's index table
+                    print(key, file = index_file)
+
+                    #save article info
+                    article = law[key]
+                    with open(f"{directory}/{key}","w") as fd:
+                        #TODO use a tokenizer that removes punctuation marks
+                        texts = [[word for word in document.lower().split() if word not in self.stoplist] for document in article]
+                        for paragraph in texts:
+                            for word in paragraph:
+                                print(word, file = fd, end = ",")
+                            print(file = fd)
 
     def load_corpus(self):
-        pass
+        laws = self.get_all_laws()
+        docs = []
+        for l_id in laws:
+            directory = st.get_law_directory(l_id)
+            for article in self.get_articles(l_id):
+                with open(f"{directory}/{article}") as article_fd:
+                    docs += [line.split(',')[:-1] for line in article_fd.readlines()]
 
-class Corpus():
-    def __iter__(self):
-        pass
+        dictionary = gs.corpora.Dictionary(docs)
+        return dictionary
+
+    def get_articles(self, law_id):
+        index = open(st.get_law_index_file(law_id))
+        arts_id = []
+        art_id = index.readline()
+        while art_id:
+            arts_id.append(art_id[:-1])
+            art_id = index.readline()
+
+        index.close()
+        return arts_id
+
+    def get_all_laws(self):
+        index = open(st.law_index_file)
+        laws_id = []
+        law_id = index.readline()
+        while law_id:
+            laws_id.append(law_id[:-1])
+            law_id = index.readline()
+
+        index.close()
+        return laws_id
+
+
+# la = LawAnalyzer(r"src\test\testing_law_1.json")
+# la.save_law(la.new_law)
+# la.save_law(la.old_law)
+# d = la.load_corpus()
+# print(d)
--- src/settings.py
+++ src/settings.py
@@ -1,10 +1,15 @@
 import os
 
 corpus_directory = "src/corpora/"
+law_index_file = f"{corpus_directory}/index.law"
+
+def get_law_index_file(law_id):
+    return f"{get_law_directory(law_id)}/index.arts"
 
 def get_law_directory(law_id):
     try:
         os.makedirs(f"{corpus_directory}/{law_id}")
+        return f"{corpus_directory}/{law_id}"
     except:
         pass
     return f"{corpus_directory}/{law_id}"
